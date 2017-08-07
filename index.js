b13d333a152c27ee613a1c8c7c1827efa6e424ee
smxlx <997234900@qq.com>
2017-08-07 23:25:58 +0800

    commit

--- index.js
+++ index.js
@@ -17,13 +17,23 @@ app.use(favicon(__dirname+"/public/favicon.ico"));
 app.set('views', path.join(__dirname, 'views'));  
 app.set('view engine', 'ejs');  
 
-app.use('(/cms)?/api/v1',proxy({
-	target:'http://127.0.0.1:3000',
-	changeOrigin:true,
-	router:{
-		'http://127.0.0.1:8000':'http://127.0.0.1:3000'
-	}
-}))
+if(process.env.BLOGFRONT =='production'){
+	app.use('(/cms)?/api/v1',proxy({
+		target:'http://127.0.0.1:3000',
+		changeOrigin:true,
+		router:{
+			'http://127.0.0.1:8000':'http://127.0.0.1:3000'
+		}
+	}))
+}else{
+	app.use('(/cms)?/api/v1',proxy({
+		target:'http://47.92.115.203:3000',
+		changeOrigin:true,
+		router:{
+			'http://47.92.115.203:8087':'http://47.92.115.203:3000'
+		}
+	}))
+}
 
 app.use(router);
 app.use(express.static(__dirname+"/public"));
@@ -35,12 +45,13 @@ app.use(function(req,res){
 	res.redirect("/not-found");
 });
 //如果本地环境
-if(process.env.GOMECARTFRONT==="dev"){
-	app.listen(8000,function(){
-		 console.log('blog app run on port http://localhost:8000  ' +app.get('env') +'  '+U.formatLong(new Date-0));
-	});
-}else{
+
+if(process.env.BLOGFRONT==="production"){
 	app.listen(8087,function(){	
 		console.log(U.formatLong(new Date-0)+"blog app run on port 8087");
+	});
+}else{
+	app.listen(8000,function(){
+		 console.log('blog app run on port http://localhost:8000  ' +app.get('env') +'  '+U.formatLong(new Date-0));
 	});
 }
